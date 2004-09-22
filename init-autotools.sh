a65eb40194bdb207efdd88a1bce22d16a0870d0b
wash <wash>
2004-09-22 10:05:13 +0000

    distribute the complete and unmodified libsvm-2.6 with the package

--- init-autotools.sh
+++ init-autotools.sh
@@ -1,6 +1,5 @@
 # Inits project from configure.ac
 aclocal
-touch stamp-h
 autoheader
-autoconf
 automake -a
+autoconf
--- rnaz/svm_helper.h
+++ rnaz/svm_helper.h
@@ -7,36 +7,9 @@
  *                                                                   *
  *	          c Stefan Washietl, Ivo L Hofacker                  *
  *                                                                   *
- *	   $Id: svm_helper.h,v 1.3 2004-09-19 13:31:41 wash Exp $    *
+ *	   $Id: svm_helper.h,v 1.4 2004-09-22 10:05:21 wash Exp $    *
  *                                                                   *
  *********************************************************************/
-
-
-/*
-  declare svm_model for other functions (declaration is only in
-  svmlib/svm.cpp and not in svmlib/svm.h)
-*/
-
-struct svm_model
-{
-  struct svm_parameter param;	// parameter 
-  int nr_class; // number of classes, = 2 in regression/one class svm
-  int l;			// total #SV
-  struct svm_node **SV;		// SVs (SV[l])
-  double **sv_coef;	// coefficients for SVs in decision functions (sv_coef[n-1][l])
-  double *rho;		// constants in decision functions (rho[n*(n-1)/2])
-  double *probA;          // pariwise probability information
-  double *probB;
-
-  // for classification only
-
-  int *label;		// label of each class (label[n])
-  int *nSV;		// number of SVs for each class (nSV[n])
-				// nSV[0] + nSV[1] + ... + nSV[n-1] = l
-  // XXX
-  int free_sv;		// 1 if svm_model is created by svm_load_model
-  // 0 if svm_model is created by svm_train
-};
 
 
 void get_regression_models(struct svm_model** avg_model,
