30906f1543efdeee1ba192b6d13b64dd0ccc86c3
wash <wash>
2008-01-24 10:19:14 +0000

    Added ploting functions and new version of RNAstrand

--- rnaz/RNAz.c
+++ rnaz/RNAz.c
@@ -7,7 +7,7 @@
  *                                                                   *
  *	          c Stefan Washietl, Ivo L Hofacker                      *
  *                                                                   *
- *	   $Id: RNAz.c,v 1.12 2006-10-12 16:58:18 wash Exp $              *
+ *	   $Id: RNAz.c,v 1.13 2008-01-24 10:19:14 wash Exp $             *
  *                                                                   *
  *********************************************************************/
 
@@ -23,12 +23,13 @@
 #include "utils.h"
 #include "pair_mat.h"
 #include "alifold.h"
+#include "PS_dot.h"
 #include "zscore.h"
 #include "rnaz_utils.h"
 #include "svm.h"
 #include "svm_helper.h"
 #include "cmdline.h"
-#include "strand.h"
+#include "strand.h" 
 
 #define IN_RANGE(LOWER,VALUE,UPPER) ((VALUE <= UPPER) && (VALUE >= LOWER))
 
@@ -38,6 +39,11 @@ PRIVATE void version(void);
 PRIVATE void classify(double* prob, double* decValue,
 					  struct svm_model* decision_model,
 					  double id,int n_seq, double z,double sci);
+
+PRIVATE void gfxPlot(char* fname, char* structure, char* consensus, struct aln *window[],int n_seq);
+PRIVATE char **annote(const char *structure, const char *AS[]);
+
+
 PRIVATE void warning(char* string, double id,int n_seq, double z,double sci,
 					 struct aln *AS[]);
 
@@ -53,7 +59,6 @@ enum {FORWARD=1, REVERSE=2};
 int main(int argc, char *argv[])
 {
 
-  char *modelDir=NULL;              /* Directory with model files */
   struct svm_model* decision_model; /* SVM classification model */
 
   /* Command line options */
@@ -62,12 +67,13 @@ int main(int argc, char *argv[])
   int showHelp=0;    /* Show short help and exits */
   int from=-1;       /* Scan slice from-to  */
   int to=-1;
-
   FILE *clust_file=stdin; /* Input file */
   FILE *out=stdout; /* Output file */
 
   struct aln *AS[MAX_NUM_NAMES];     
   struct aln *window[MAX_NUM_NAMES]; 
+  struct aln *windowFwdMem[MAX_NUM_NAMES];
+
   char *tmpAln[MAX_NUM_NAMES];
 
   int n_seq;  /* number of input sequences */
@@ -75,8 +81,9 @@ int main(int argc, char *argv[])
 
   char *structure=NULL;
   char *singleStruc,*gapStruc, *output,*woGapsSeq;
-  char strand[8];
+  char strandString[8];
   char warningString[1000];
+  char gfxName[100];
   char *string=NULL;
   double singleMFE,sumMFE,singleZ,sumZ,z,sci,id,decValue,prob,comb;
   double min_en, real_en;
@@ -91,8 +98,11 @@ int main(int argc, char *argv[])
   double consensusMFE_fwd=0;
   double sci_fwd=0;
   double z_fwd=0;
+  double gu=0;
+  double gu_fwd=0;
+
   int strandGuess;
-  double strandProb,strandDec;
+  double strandScore,strandProb,strandDec;
   
   if (cmdline_parser (argc, argv, &args) != 0){
 	usage();
@@ -163,22 +173,15 @@ int main(int argc, char *argv[])
   switch(checkFormat(clust_file)){
 
   case CLUSTAL:
-	readFunction=&read_clustal;
+	readFunction=&readClustal;
 	break;
   case MAF:
-	readFunction=&read_maf;
+	readFunction=&readMaf;
 	break;
   case 0:
 	nrerror("ERROR: Unknown alignment file format. Use Clustal W or MAF format.\n");
   }
 
-  /* modelDir=getenv("RNAZDIR");
-  if (modelDir==NULL){
-  nrerror("ERROR: Could not find models. You have to set the RNAZDIR"
-          " enviroment variable pointing to the model files!\n");
-  }*/
-
- 
   decision_model=get_decision_model(NULL);
   regression_svm_init(NULL);
   
@@ -229,9 +232,9 @@ int main(int argc, char *argv[])
 	  
 	  if (currDirection==REVERSE){
 		revAln((struct aln **)window);
-		strcpy(strand,"reverse");
+		strcpy(strandString,"reverse");
 	  } else {
-		strcpy(strand,"forward");
+		strcpy(strandString,"forward");
 	  }
 
 	  structure = (char *) space((unsigned) length+1);
@@ -322,7 +325,7 @@ int main(int argc, char *argv[])
 		real_en = s/i;
 	  }
 
-	  string = consensus((const struct aln**) window);
+	  string = consensusSeq((const struct aln**) window);
 	  sprintf(output+strlen(output),
 			  ">consensus\n%s\n%s (%6.2f = %6.2f + %6.2f) \n",
 			  string, structure, min_en, real_en, min_en-real_en );
@@ -364,7 +367,7 @@ int main(int argc, char *argv[])
 		fprintf(out," Slice: %u to %u\n",from,to);
 	  }
 	  fprintf(out," Columns: %u\n",length);
-	  fprintf(out," Reading direction: %s\n",strand);
+	  fprintf(out," Reading direction: %s\n",strandString);
 	  fprintf(out," Mean pairwise identity: %6.2f\n", id);
 	  fprintf(out," Mean single sequence MFE: %6.2f\n", sumMFE/n_seq);
 	  fprintf(out," Consensus MFE: %6.2f\n",min_en);
@@ -389,30 +392,69 @@ int main(int argc, char *argv[])
 	
 	  fflush(out);
 
-	  free(structure);
-	  free(output);
+	  if (args.plot_given){
+		if (countAln==1){
+		  strcpy(gfxName,".ps");
+		} else {
+		  sprintf(gfxName,"%i.ps",countAln-1);
+		}
+		gfxPlot(gfxName, structure, string, window, n_seq);
+	  }
+ 
+
+	  
+	 
+
+	  
 
 	  if (currDirection==FORWARD && args.predict_strand_flag){
-		meanMFE_fwd=sumMFE/n_seq;
-		consensusMFE_fwd=min_en;
-		sci_fwd=sci;
-		z_fwd=z;
+
+      sliceAln((const struct aln**)window, (struct aln **)windowFwdMem, 1, length);
+
+      meanMFE_fwd=sumMFE/n_seq;
+      consensusMFE_fwd=min_en;
+      sci_fwd=sci;
+      z_fwd=z;
+      
+      string = (char*)consensusSeq((const struct aln**) window);
+      gu_fwd=(double)frequency_gu_pairs(string, structure);
+      
 	  }
 
 	  if (currDirection==REVERSE && args.predict_strand_flag){
 
-		if (predict_strand(sci_fwd-sci, meanMFE_fwd-(sumMFE/n_seq),
-						   consensusMFE_fwd-min_en, z_fwd-z, n_seq, id, 
-						   &strandGuess, &strandProb, &strandDec, NULL)){
-		  if (strandGuess==1){
-			fprintf(out, "\n# Strand winner: forward (%.2f)\n",strandProb);
-		  } else {
-			fprintf(out, "\n# Strand winner: reverse (%.2f)\n",1-strandProb);
-		  }
-		} else {
-		  fprintf(out, "\n# WARNING: No strand prediction (values out of range)\n");
-		}
+      string = (char*)consensusSeq((const struct aln**) window);
+      gu=(double)frequency_gu_pairs(string, structure);
+
+
+      // strand(double deltaSCI, double deltaMeanMFE, double deltaConsMFE, double deltaZ, int n_seq, double id, double consFreqGU, 
+      //            double *score, double *prob, double *decValue, char *modelDir, int code)
+
+      printf("inhere\n");
+
+      if (strand(sci_fwd-sci, meanMFE_fwd-(sumMFE/n_seq), 
+                 consensusMFE_fwd-min_en, z_fwd-z, n_seq, id, gu_fwd+gu,
+                 &strandScore, &strandProb, &strandDec, NULL,CODE_SCI_Z_MEANMFE_CONSMFE)){
+
+
+      /* if (predict_strand(sci_fwd-sci, meanMFE_fwd-(sumMFE/n_seq), */
+/*                          consensusMFE_fwd-min_en, z_fwd-z, n_seq, id,  */
+/*                          &strandGuess, &strandProb, &strandDec, NULL)){ */
+
+       
+        if (strandScore>1){
+          fprintf(out, "\n# Strand winner: forward (%.6f)\n",strandDec);
+        } else {
+          fprintf(out, "\n# Strand winner: reverse (%.6f)\n",strandDec);
+        }
+      } else {
+        fprintf(out, "\n# WARNING: No strand prediction (values out of range)\n");
+      }
 	  }
+
+    free(structure);
+    free(output);
+
 	}
 	freeAln((struct aln **)AS);
 	freeAln((struct aln **)window);
@@ -474,6 +516,127 @@ PRIVATE void classify(double* prob, double* decValue,
   
   
 }
+/********************************************************************
+ *                                                                  *
+ * gfxPlot  -- Generates color annotated alignment and RNA          *
+ * strucutre as PostScript                                          *  
+ *                                                                  *
+ ********************************************************************
+ *                                                                  *
+ * name ... end of filename                                         *
+ * structure ... Consensus structure in dot/bracket notation        *
+ * consensus ... Consensus sequence                                 *
+ * window ... alignment structure                                   *
+ * n_seq  ... number of sequences in the alignment                  *
+ *                                                                  *
+ ********************************************************************/
+
+void gfxPlot(char* name, char* structure, char* consensus, struct aln *window[],int n_seq){ 
+
+  char **A;
+  char fname[128];
+  int k;
+  char **seqs;
+  char **names;
+
+  seqs=(char**)space(sizeof(char*)*(n_seq+1));
+  names=(char **)space(sizeof(char*)*(n_seq+1));
+
+  for (k=0;k<n_seq;k++){
+    seqs[k]=window[k]->seq;
+    names[k]=window[k]->name;
+  }
+
+  sprintf(fname,"rna%s",name); 
+
+  A = annote(structure, (const char**) seqs);
+
+  (void) PS_rna_plot_a(consensus, structure, fname, A[0], A[1]);
+
+  sprintf(fname,"aln%s",name);
+
+  PS_color_aln(structure, fname,(const char**) seqs,(const char**) names);
+
+  free(A[0]);
+  free(A[1]);
+  free(seqs);
+  free(names);
+  
+}
+
+
+PRIVATE char **annote(const char *structure, const char *AS[]) {
+  char *ps, *colorps, **A;
+  int i, n, s, pairings, maxl;
+  short *ptable;
+  char * colorMatrix[6][3] = {
+    {"0.0 1", "0.0 0.6",  "0.0 0.2"},  /* red    */
+    {"0.16 1","0.16 0.6", "0.16 0.2"}, /* ochre  */
+    {"0.32 1","0.32 0.6", "0.32 0.2"}, /* turquoise */
+    {"0.48 1","0.48 0.6", "0.48 0.2"}, /* green  */
+    {"0.65 1","0.65 0.6", "0.65 0.2"}, /* blue   */
+    {"0.81 1","0.81 0.6", "0.81 0.2"} /* violet */
+  };
+
+  make_pair_matrix();
+  n = strlen(AS[0]);
+  maxl = 1024;
+
+  A = (char **) space(sizeof(char *)*2);
+  ps = (char *) space(maxl);
+  colorps = (char *) space(maxl);
+  ptable = make_pair_table(structure);
+  for (i=1; i<=n; i++) {
+    char pps[64], ci='\0', cj='\0';
+    int j, type, pfreq[8] = {0,0,0,0,0,0,0,0}, vi=0, vj=0;
+    if ((j=ptable[i])<i) continue;
+    for (s=0; AS[s]!=NULL; s++) {
+      type = pair[encode_char(AS[s][i-1])][encode_char(AS[s][j-1])];
+      pfreq[type]++;
+      if (type) {
+	if (AS[s][i-1] != ci) { ci = AS[s][i-1]; vi++;}
+	if (AS[s][j-1] != cj) { cj = AS[s][j-1]; vj++;}
+      }
+    }
+    for (pairings=0,s=1; s<=7; s++) {
+      if (pfreq[s]) pairings++;
+    }
+
+    if ((maxl - strlen(ps) < 192) || ((maxl - strlen(colorps)) < 64)) {
+      maxl *= 2;
+      ps = realloc(ps, maxl);
+      colorps = realloc(colorps, maxl);
+      if ((ps==NULL) || (colorps == NULL))
+	  nrerror("out of memory in realloc");
+    }
+
+    if (pfreq[0]<=2) {
+      snprintf(pps, 64, "%d %d %s colorpair\n",
+	       i,j, colorMatrix[pairings-1][pfreq[0]]);
+      strcat(colorps, pps);
+    }
+
+    if (pfreq[0]>0) {
+      snprintf(pps, 64, "%d %d %d gmark\n", i, j, pfreq[0]);
+      strcat(ps, pps);
+    }
+    if (vi>1) {
+      snprintf(pps, 64, "%d cmark\n", i);
+      strcat(ps, pps);
+    }
+    if (vj>1) {
+      snprintf(pps, 64, "%d cmark\n", j);
+      strcat(ps, pps);
+    }
+  }
+  free(ptable);
+  A[0]=colorps;
+  A[1]=ps;
+  return A;
+}
+
+
+
 
 /*Hardcoded limits a more sophisticated data-model for meta-model information should be
   considered*/
@@ -586,7 +749,8 @@ PRIVATE void help(void){
   printf("%s\n","  -w, --window=START-STOP Score window from START to STOP");
   printf("%s\n","  -p, --cutoff=FLOAT      Probability cutoff");
   printf("%s\n","  -g, --show-gaps         Display alignment with gap  (default=off)");
-  printf("%s\n\n","  -s, --predict-strand    Use strand predictor  (default=off)");
+  printf("%s\n","  -s, --predict-strand    Use strand predictor  (default=off)");
+  printf("%s\n\n","  -x, --plot              Generate graphical output  (default=off)");
 
 }
 
