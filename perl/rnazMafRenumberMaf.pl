9a1c03bd11d7b30c146c72488b1d94ff7a740e10
Andrea Tanzer <at@tbi.univie.ac.at>
2013-08-28 18:00:44 +0200

    new script: perl/rnazMafRenumberMaf.pl
    I ran conversion of maf alignments into gtf files describing the alignment
    blocks separately for each chromosome. thus, each chromosome has it's
    own numbering of blocks and the blockNr/index is not
    continuous for the entire genome.
    rnazMafRenumberMaf.pl fixes this problem. it numerically goeas through
    chromosomes (chr1-19,X,Y,M), reads the last block nr of the previous
    chromosome (i-1) and adds it to the block number listed in the i-th chromosome gtf files.

---
+++ perl/rnazMafRenumberMaf.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+# Last Time-stamp: <2013-08-28 16:58:39 at>
+# date-o-birth: <2013-08-28 11:18:45 at>, Vienna
+# renumber maf block numbering
+
+use strict;
+use warnings;
+
+
+
+
+
+# --------------------------------------------------
+
+my $max = 19;
+
+for my $i (10..$max){
+
+  my $c_file = "chr".$i.".maf.gtf";
+  my $p_file = "chr".($i-1).".maf.gtf";
+  my $o_file = "chr".$i.".maf.gtf";
+  
+  my $prev = `tail -1  $p_file`;
+
+  my $c = $1 if $prev =~ /blockNr \"(\S+)\"/;
+  print "blockNr $c\n";
+
+  open(IN,  "<../$c_file") || die "could not open $c_file: $!\n";
+  open(OUT, ">$o_file") || die "could not open $o_file: $!\n";
+      
+  while(<IN>){
+    next if /^\s+$/ || /^\#/;
+    chomp;
+    
+    my $cc = $1 if /blockNr \"(\S+)\"/;
+#    print OUT "blockNr $c $cc ";
+    $cc += $c;
+    s/(blockNr) \"(\S+)\"/$1 \"$cc\"/;
+#    print OUT "$cc";
+    print OUT "$_\n";
+  }
+  close(OUT);
+  close(IN);
+}
+
